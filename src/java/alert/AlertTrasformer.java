0ba06b0e6a0a7b3fa7a891322aedea1e11fc2ac3
MatveyYR <102485396+MatveyYR@users.noreply.github.com>
2024-02-10 19:35:26 +0300

    Update AlertTrasformer.java

--- src/java/alert/AlertTrasformer.java
+++ src/java/alert/AlertTrasformer.java
@@ -1,4 +1,4 @@
-package im.manokhin.alerter;
+package alerter;
 
 import org.apache.avro.LogicalTypes;
 import org.apache.avro.Schema;
@@ -15,7 +15,7 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
-import static im.manokhin.alerter.EarningAlertsApp.STATE_STORE_NAME;
+import static alerter.EarningAlertsApp.STATE_STORE_NAME;
 
 public class EarningAlertTrasformer implements Processor<String, GenericRecord, String, GenericRecord> {
     public static final Duration ONE_MINUTE = Duration.ofMinutes(1);
@@ -33,13 +33,11 @@ public class EarningAlertTrasformer implements Processor<String, GenericRecord, 
     @Override
     public void process(Record<String, GenericRecord> record) {
         long timestamp = record.timestamp();
-        // округляем наш таймстемп вниз до ближайшей минуты - получаем начало окна
         long nearestMinutesTs = timestamp - timestamp % 60_000;
         String productId = record.value().get("product_id").toString();
         Long quantity = (Long) record.value().get("purchase_quantity");
         Double price = (Double) record.value().get("product_price");
         Double earnings = quantity * price;
-        // создаем ключ в сторе конкатенации начала окна и id продукта
         byte[] stateStoreKey = createKey(nearestMinutesTs, productId);
         Double oldVal = stateStore.get(stateStoreKey);
         double newVal = oldVal != null ? oldVal + earnings : earnings;
@@ -52,35 +50,23 @@ public class EarningAlertTrasformer implements Processor<String, GenericRecord, 
 
     private void sendAlerts(long timestamp) {
         long nearestMinutesTs = timestamp - timestamp % 60_000;
-        // в RocksDB, которая является стором для Kafka Streams, записи лежат в отсортированном виде
-        // тогда при запросе записей range запросом, мы получим ответ быстро
-        // если интересно побольше прочитать, как устроены key-value хранилища и конкретно RocksDB
-        // смотри https://www.confluent.io/blog/how-to-tune-rocksdb-kafka-streams-state-stores-performance/
-        // а также Martin Kleppmann - Designing Data Intensive Applications, глава 3, SSTables
         stateStore.range(createKey(lastProcessedWindowEnd, ""), createKey(nearestMinutesTs, ""))
                         .forEachRemaining(keyVal -> {
                             long ts = extractTsFromKey(keyVal.key);
                             String productId = extractProductIdFromKey(keyVal.key);
                             Double earnings = keyVal.value;
                             if (earnings > EarningAlertsApp.MAX_EARNINGS_PER_MINUTE) {
-                                // создаем схему нашего алерта
                                 Schema schema = SchemaBuilder.record("QuantityAlert").fields()
                                         .name("window_start")
-                                        // AVRO допускает использование "логических" типов
-                                        // в данном случае мы показываем, что в данном поле лежит таймстемп
-                                        // в миллисекундах epoch
                                         .type(LogicalTypes.timestampMillis().addToSchema(Schema.create(Schema.Type.LONG)))
                                         .noDefault()
                                         .requiredDouble("earnings")
                                         .endRecord();
                                 GenericRecord record = new GenericData.Record(schema);
-                                // старт окна у нас в миллисекундах
                                 record.put("window_start", ts);
                                 record.put("earnings", earnings);
                                 context.forward(new Record<>(productId, record, ts));
                             }
-                            // удаляем ключ из стора, так как по этому окну уже обработаны записи для этого ключа
-                            // чтобы исключить повторную обработку
                             stateStore.delete(keyVal.key);
                         });
         lastProcessedWindowEnd = nearestMinutesTs;
@@ -98,7 +84,6 @@ public class EarningAlertTrasformer implements Processor<String, GenericRecord, 
     }
 
     private byte[] createKey(long nearestMinutesTs, String productId) {
-        // создаем ключ конкатенацией начала окна и productId
         byte[] key = new byte[Long.BYTES + productId.length()];
         System.arraycopy(
                 ByteBuffer.allocate(Long.BYTES).putLong(nearestMinutesTs).array(),
